92b3b3c1b2bfcb1cf7c1a4097746ab5fb2188f9b
Ruslan <pixel.365.24@gmail.com>
2021-07-26 07:03:00 +0300

    tests

--- tests/test_clico.py
+++ tests/test_clico.py
@@ -1,11 +1,10 @@
-import unittest
-from unittest import mock
+from unittest import mock, TestCase
 
 from clico import Clico
-from clico.models import Domain, Link, Error, Result
+from clico.models import Domain, Link, Error, Result, Integration
 
 
-class TestClico(unittest.TestCase):
+class TestClico(TestCase):
     def setUp(self):
         self.clico = Clico(api_key='api_key')
         self.valid_link = {
@@ -15,6 +14,13 @@ class TestClico(unittest.TestCase):
             'is_deep': False,
             'id_campaign': 'my-campaign',
             'utm_phone': 'XXXXXXXXXXX',
+        }
+        self.valid_integration = {
+            'callback_uri': 'https://my-api.com/callback',
+            'redirect_uri': 'https://google.com',
+            'user_data': {
+                'key': 'value'
+            }
         }
 
     def test_empty_api_key(self):
@@ -88,7 +94,30 @@ class TestClico(unittest.TestCase):
         ])
         self.assertIsInstance(multiple_links, Error)
 
-    def test_integration(self):
+    def test_integration_assertion_failed(self):
         with self.assertRaises(AssertionError) as ctx:
             self.clico.integration('')
         self.assertEqual(str(ctx.exception), 'Type `str` is not an instance of `Integration` object')
+
+    @mock.patch("clico.clico.requests.post")
+    def test_integration_success(self, mock_post):
+        token = 'https://my.cli.com/?token=c2c31eca-b7e0-4a73-85a4-6e0975b81d76'
+        fake_result = Result(result=token)
+
+        mock_response = mock.Mock(status_code=200)
+        mock_response.json.return_value = fake_result.dict()
+        mock_post.return_value = mock_response
+
+        integration = self.clico.integration(Integration(**self.valid_integration))
+        self.assertEqual(integration.token, token)
+
+    @mock.patch("clico.clico.requests.post")
+    def test_integration_failed(self, mock_post):
+        mock_response = mock.Mock(status_code=400)
+        mock_response.json.return_value = {
+            "status": 400
+        }
+        mock_post.return_value = mock_response
+
+        multiple_links = self.clico.integration(Integration(**self.valid_integration))
+        self.assertIsInstance(multiple_links, Error)
